61d3cb3fc84e9511173a6286dfc9f3b8e86b2218
dev-jb-007 <devpatel8907@gmail.com>
2021-05-15 20:26:33 +0530

    snake game

---
+++ index.js
@@ -0,0 +1,155 @@
+//Variables
+const foodmusic=new Audio("music/food.mp3");
+const gameovermusic=new Audio("music/gameover.mp3");
+const movemusic=new Audio("music/move.mp3");
+console.log("Hi");
+let lasttime = 0;
+let speed = 10;
+let direction = { x: 0, y: 0 };
+let snakearr = [{ x: 13, y: 15 }];
+let board = document.querySelector(".board");
+let food = { x: 5, y: 5 };
+let score=0;
+let scoredisplay=document.querySelector(".score");
+let highscore=0;
+//Display High Score
+// function HighScore(){
+//     let highscoreobj=localStorage.getItem("highscore");
+//     if(highscoreobj==NULL){
+//         highscoreobj=JSON.stringify(0);
+//     }
+//     else{
+//         if(highscoreobj<score)
+//         {
+//             highscoreobj=JSON.stringfy(score);
+//         }
+//     }
+//     localStorage.setItem("highscore",highscoreobj);
+//     document.getElementById("highscore_id").innerHTML=`High Score:${JSON.parse(highscoreobj)}`;
+// }
+
+
+//Game  creaction
+function isCollide() {
+    for(let i = 1; i <snakearr.length; i++) {
+        if(snakearr[0].x==snakearr[i].x&&snakearr[0].y==snakearr[i].y)
+        {
+            return true;
+        }
+    }
+        if(snakearr[0].x>=19||snakearr[0].x<=0||snakearr[0].y>=19||snakearr[0].y<=0)
+        {
+            return true;
+        }
+        return false;
+}
+function foodproblem(){
+    let fooddiv = document.createElement("div");
+    fooddiv.classList.add("food");
+    fooddiv.style.gridRowStart = food.y;
+    fooddiv.style.gridColumnStart = food.x;
+    board.appendChild(fooddiv);
+}
+
+function rungame() {
+    if(isCollide()==true)
+    {   gameovermusic.play();
+        alert("Your game is over");
+        direction = { x: 0, y: 0 };
+        snakearr = [{ x: 13, y: 15 }];
+        food = { x: 5, y: 5 };
+    }
+    //Add snake and dispaly snake
+    board.innerHTML = '';
+    snakearr.forEach((e, index) => {
+        if (index == 0) {
+            let element = document.createElement("div");
+            element.classList.add("head");
+            element.style.gridRowStart = e.y;
+            element.style.gridColumnStart = e.x;
+            board.appendChild(element);
+        }
+        else{
+            let element = document.createElement("div");
+            element.classList.add("snake");
+            element.style.gridRowStart = e.y;
+            element.style.gridColumnStart = e.x;
+            board.appendChild(element);
+        }
+
+    });
+    //Add food
+    foodproblem();
+    if (snakearr[0].x == food.x && snakearr[0].y == food.y) {
+        foodmusic.play();
+        let a = snakearr[0].x + direction.x;
+        let b = snakearr[0].y + direction.y;
+        snakearr.unshift({ x: a, y: b });
+        food={x:(Math.round(0+(18)*Math.random())),y:(Math.round(0+(18)*Math.random()))};
+        score++;
+        speed++;
+        scoredisplay.innerHTML=`<h1>Score:${score}</h1>`;
+    }
+    
+    //Moving snake
+    for (let i = snakearr.length - 2; i >= 0; i--) {
+        snakearr[i + 1] = { ...snakearr[i] };
+    }
+    snakearr[0].x += direction.x;
+    snakearr[0].y += direction.y;
+
+
+
+
+
+    //Moving Snake
+}
+window.addEventListener("keydown", (e) => {
+    movemusic.play();
+    direction = { x: 0, y: 1 };
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("arrow up");
+            direction.x = 0;
+            direction.y = -1;
+            break;
+        case "ArrowDown":
+            console.log("Down");
+            direction.x = 0;
+            direction.y = 1;
+            break;
+        case "ArrowLeft":
+            console.log("arrow left");
+            direction.x = -1;
+            direction.y = 0;
+            break;
+        case "ArrowRight":
+            console.log("arrow right");
+            direction.x = 1;
+            direction.y = 0;
+            break;
+
+        default:
+            break;
+    }
+})
+
+
+
+
+
+//Setting animations
+function move(giventime) {
+    window.requestAnimationFrame(move);
+    if ((giventime - lasttime) / 1000 < 1 / speed) {
+        return;
+    }
+    lasttime = giventime;
+    rungame();
+}
+
+
+
+
+//Main
+window.requestAnimationFrame(move);
